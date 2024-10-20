6738797f289a63daf5a482e5ce76738d133f6112
CommandBeat <agabaradford@gmail.com>
2024-10-21 09:08:17 +1300

    Create conversation_saver_basics(not in the main code).py

---
+++ conversation_saver_basics(not in the main code).py
@@ -0,0 +1,10 @@
+import os
+
+def save(user):
+    user_input = input("Hello! What seems to be the problem? ")
+    file_name = user_input + ".txt"
+    dest = "D:/Conversations"
+    folder = os.makedirs(dest, exist_ok=True)
+    path = os.path.join(dest, file_name)
+    with open(path, 'w') as file:
+        file.write(user_input + ".")
