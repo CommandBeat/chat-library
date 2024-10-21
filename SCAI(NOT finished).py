67bf45b75a86ab3c85359f436e8a257991ebd224
CommandBeat <agabaradford@gmail.com>
2024-10-22 10:41:04 +1300

    Remade

--- SCAI(NOT finished).py
+++ SCAI(NOT finished).py
@@ -10,16 +10,16 @@ import sys as s
 directory_downloads = "C:/ChatBot-1.0.0/Downloads/Packages"
 directory_conversation_logger = "C:/ChatBot-1.0.0/Conversations"
 url = 'https://raw.githubusercontent.com/CommandBeat/chat-library/refs/heads/SCAI(Super-Cool-Artificial-Intelligence)/variable_safe.py'
+"""
 conversation_logger_folder = os.makedirs(directory_conversation_logger)
 download_folder = os.makedirs(directory_downloads, exist_ok=True)
 current_time = t.time()
 if not os.path.exists(directory_conversation_logger):
     download_folder = os.makedirs(directory_conversation_logger, exist_ok=True)
-else:
-    raise FileExistsError("[WinError 183] Cannot create a file when that file already exists: 'C:/ChatBot-1.0.0/Downloads/Packages'")
 random = r.randint(0, 100000000)
 backup_random = r.randint(0, 100000000)
 items = os.listdir(directory_conversation_logger)
+"""
 
 # renaming "-" to "_"/ hyphen to underscore
 def rename_files(directory_conversation_logger):
@@ -34,9 +34,10 @@ dir = "C:/Conversations"
 current_time = t.time()
 replies = {"...", "..", ".", ""}
 database = {}
-random = r.randint(0, 100000000)
-backup_random = r.randint(0, 100000000)
 """
+random = r.randint(0, 100000000)
+# backup_random = r.randint(0, 100000000)
+
 
 # Scanning for any duplicates
 def scan_directory(directory_conversation_logger):
@@ -68,3 +69,6 @@ def process(user_input, directory_conversation_logger, database):
 def reply(database):
     database = {{random}}
     print("Chat was successfully saved!")
+    print(database)
+
+reply(database)
